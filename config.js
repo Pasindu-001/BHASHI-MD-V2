cfedb952b40c7b7c4e8f89383f83f9dda347e115
Pasindu-001 <pasindulk832@gmail.com>
2025-02-05 05:27:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "BHASHI-MD~32JA2KJR#3vo-OerpdLYDlu6uqF-kYHslFqOB0w8I5ZCNXO6JQmU",
+  SESSION_ID: process.env.SESSION_ID || "BHASHI-MD~0bcBRRqT#nGUOjSu_ptubD8J8GXz4bsbm5-Ujzt8pixmD_vASonQ",
 // For extra thing : BHASHI-MD~(megafileid)
   MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 
